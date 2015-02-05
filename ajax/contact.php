3b26b96e2ca558573fb461baf67203821229dc9f
Xavier Gonzalez <xavier@gonzalez.pm>
2015-02-05 15:24:52 +0100

    Modif email avec plain text +mail client

--- ajax/contact.php
+++ ajax/contact.php
@@ -1,44 +1,41 @@
 <?php
+error_log ( date ( "Y-m-d H:i:s" ) . " : " . $_POST ['action'] . "\n", 3, "../log/spy.log" );
+error_log ( date ( "Y-m-d H:i:s" ) . " : " . $_POST ['name'] . "\n", 3, "../log/spy.log" );
+error_log ( date ( "Y-m-d H:i:s" ) . " : " . $_POST ['horaire'] . "\n", 3, "../log/spy.log" );
+error_log ( date ( "Y-m-d H:i:s" ) . " : " . $_POST ['email'] . "\n", 3, "../log/spy.log" );
+error_log ( date ( "Y-m-d H:i:s" ) . " : " . $_POST ['tel'] . "\n", 3, "../log/spy.log" );
+error_log ( date ( "Y-m-d H:i:s" ) . " : " . $_POST ['message'] . "\n", 3, "../log/spy.log" );
+error_log ( date ( "Y-m-d H:i:s" ) . " : " . $_POST ['cp'] . "\n", 3, "../log/spy.log" );
 
-error_log(date("Y-m-d H:i:s") ." : ". $_POST['action'] ."\n", 3, "../log/spy.log");
-error_log(date("Y-m-d H:i:s") ." : ". $_POST['name'] ."\n", 3, "../log/spy.log");
-error_log(date("Y-m-d H:i:s") ." : ". $_POST['horaire'] ."\n", 3, "../log/spy.log");
-error_log(date("Y-m-d H:i:s") ." : ". $_POST['email'] ."\n", 3, "../log/spy.log");
-error_log(date("Y-m-d H:i:s") ." : ". $_POST['tel'] ."\n", 3, "../log/spy.log");
-error_log(date("Y-m-d H:i:s") ." : ". $_POST['message'] ."\n", 3, "../log/spy.log");
-error_log(date("Y-m-d H:i:s") ." : ". $_POST['cp'] ."\n", 3, "../log/spy.log");
-
-
-if ($_POST["action"] == "sendMail") {
-
+if ($_POST ["action"] == "sendMail") {
+	
 	$_to = "edenblue33@gmail.com";
-	//$_to = "fjavi.gonzalez@gmail.com";
+	// $_to = "fjavi.gonzalez@gmail.com";
+	// $_to = "web-MprMi1@mail-tester.com";
 	$sujet = "Eden Blue - Contact Site";
-	//echo "Envoi du message à " . $_to . "<br>";
-
-	$entete = "From:EdenBlue <edenblue33@gmail.com>\n";
+	
+	$entete = "From:EdenBlue <contact@edenblue.fr>\n";
 	$entete .= "MIME-version: 1.0\n";
-	$entete .= "Content-type: text/html; charset= iso-8859-1\n";
-	$entete .= "Bcc: fjavi.gonzalez@gmail.com, xav335@hotmail.com\n";
-
+	$entete .= "Content-type: text/plain; charset= iso-8859-1\n";
+	$entete .= "Bcc: fjavi.gonzalez@gmail.com\n";
+	
 	$corps = "";
-	$corps .= "Bonjour,<br>";
-	$corps .= "Nv message de :<br>" . $_POST["name"] . " (" . $_POST["email"] . ")<br>";
-	$corps .= "Tel : ". $_POST["tel"] ."<br>";
-	$corps .= "Code postal : ". $_POST["cp"] ."<br>";
-	$corps .= "Horaire souhaité de rappel: " . $_POST["horaire"] ."<br>";
-	$corps .= "<b>Message :</b><br>";
-	$corps .= $_POST["message"] . "<br><br>";
-	$corps = utf8_decode( $corps );
-	//echo $corps . "<br>";
-
+	$corps .= "Bonjour,\n";
+	$corps .= "Nv message de :\n" . $_POST ["name"] . " (" . $_POST ["email"] . ")\n";
+	$corps .= "Tel : " . $_POST ["tel"] . "\n";
+	$corps .= "Code postal : " . $_POST ["cp"] . "\n";
+	$corps .= "Horaire souhaité de rappel: " . $_POST ["horaire"] . "\n";
+	$corps .= "<b>Message :\n";
+	$corps .= $_POST ["message"] . "\n";
+	$corps = utf8_decode ( $corps );
+	
 	// Envoi des identifiants par mail
-	mail($_to, $sujet, stripslashes($corps), $entete);
-
-	error_log(date("Y-m-d H:i:s") ." : Message envoyé ! \n", 3, "../log/spy.log");
+	mail ( $_to, $sujet, stripslashes ( $corps ), $entete );
+	
+	error_log ( date ( "Y-m-d H:i:s" ) . " : Message envoyé ! \n", 3, "../log/spy.log" );
 } else {
-	error_log(date("Y-m-d H:i:s") ." : Message NON envoyé ! \n", 3, "../log/spy.log");
+	error_log ( date ( "Y-m-d H:i:s" ) . " : Message NON envoyé ! \n", 3, "../log/spy.log" );
 }
-error_log(date("Y-m-d H:i:s") ." xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx  \n", 3, "../log/spy.log");
+error_log ( date ( "Y-m-d H:i:s" ) . " xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx  \n", 3, "../log/spy.log" );
 
 ?>
